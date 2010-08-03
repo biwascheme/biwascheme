09306c347f53630d2f5b32ac57c6c17958739635
Masatake YAMATO <yamato@redhat.com>
2010-08-04 03:34:41 +0900

    Handle the case that bs-console doesn't exist.

--- src/console/web-console.js
+++ src/console/web-console.js
@@ -1,10 +1,15 @@
 Console = {}
 
 Console.puts = function(str, no_newline) {
-    var text = (str + (no_newline ? "" : "\n")).escapeHTML();
-    span = document.createElement("span");
-    span.innerHTML = text.replace(/\n/g,"<br>").replace(/ /g,"&nbsp;");
-    $('bs-console').insert(span);
+    var console;
+    var text
+    console = $('bs-console');
+    if (console) {
+	text = (str + (no_newline ? "" : "\n")).escapeHTML();
+	span = document.createElement("span");
+	span.innerHTML = text.replace(/\n/g,"<br>").replace(/ /g,"&nbsp;");
+	console.insert(span);
+    }
 }
 
 Console.p = function (/*ARGS*/){
