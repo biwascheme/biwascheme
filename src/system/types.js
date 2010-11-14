86d8f1368c1757cc655b2c7e2ecc9fa7bf78dfbc
Eric Jiang <erjiang@indiana.edu>
2010-11-14 17:16:49 -0500

    Fix isList to check recursively.
    BiwaScheme.isList (also used by assert_list) only checked to see if the
    obj was a pair.  It now properly checks the list recursively, ending
    with the (new) empty list.  It still does not check if it's cyclic.
    
    This is believed to cause a few test cases to fail where the test cases
    were relying on the old, incorrect implementation of assert_list.

--- src/system/types.js
+++ src/system/types.js
@@ -29,8 +29,10 @@ BiwaScheme.isPair = function(obj){
 
 // Note: isList returns true for '()
 BiwaScheme.isList = function(obj){
-  return (obj instanceof BiwaScheme.Pair);
-  // should check it is proper and not cyclic..
+    if(obj === BiwaScheme.nil) return true; // null base case
+    if(!(obj instanceof BiwaScheme.Pair)) return false;
+    return BiwaScheme.isList(obj.cdr);
+  //TODO: should check if it is not cyclic..
 };
 
 BiwaScheme.isVector = function(obj){
