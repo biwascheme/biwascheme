e38ea0347396b31f47d3f3f62e7d0b819e76d69a
Yutaka HARA <yutaka.hara.gmail.com>
2011-07-07 22:55:10 +0900

    version 0.5.6

--- src/version.js
+++ src/version.js
@@ -1,16 +1,4 @@
 var BiwaScheme = BiwaScheme || {};
 
-BiwaScheme.Version = "0.5.5.2";
-BiwaScheme.GitCommit = "7b9130dec4df7d110483f2525c52c7fd3fd16064";
-
-  // FIXME: used by js-load. should be moved
-BiwaScheme.require = function(src, check, proc){
-  var script = document.createElement('script')
-  script.src = src;
-  document.body.appendChild(script);
-
-  var checker = new Function("return !!(" + check + ")");
-
-  if(checker()) proc();
-  else          setTimeout(function(){ checker() ? proc() : setTimeout(arguments.callee, 10); }, 10);
-};
+BiwaScheme.Version = "0.5.6";
+BiwaScheme.GitCommit = "134c56e16e43e65fc18808ad8496a25a57c757be";
