9cc684aa8698a919f504227fa281da9668984180
Ken Pratt <ken@kenpratt.net>
2011-07-14 12:08:34 -0700

    Added Makefile target to build a node.js module.

---
+++ src/server/node_module_postamble.js
@@ -0,0 +1,12 @@
+var fs = require('fs');
+
+BiwaScheme.run = function(filename) {
+  var src = fs.readFileSync(filename, 'utf8');
+  var intp = new BiwaScheme.Interpreter(function(e){
+    sys.puts(e.stack);
+    process.exit(1);
+  });
+  return intp.evaluate(src);
+};
+
+exports.BiwaScheme = BiwaScheme;
