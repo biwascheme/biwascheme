eaf34cf6d4504d1b8b2e2ba149e9c7b400f4cb8a
Ken Pratt <ken@kenpratt.net>
2011-07-27 16:50:59 -0700

    Removed unused variable declarations from node & spidermonkey loaders.

--- src/console/spidermonkey-console.js
+++ src/console/spidermonkey-console.js
@@ -1,50 +1,16 @@
-window = {
-};
+// TODO load underscore and underscore.js
 
-navigator = {
-    userAgent: "console-spidermonkey",
-};
-
-document = {
-    createElement: function(a) { 
-	return {
-	    appendChild: function() {
-		return "dummy_document_createElement_appendChild";
-	    },
-	} ;
-    },
-    createTextNode: function(a) {
-	return "dummy_document_createTextNode";
-    },
-    createEvent: function(a) {
-	return "dummy_document_createEvent";
-    },
-    write: function(a) {
-	return "dummy_document_wirte";
-    },
-    getElementById: function(a) {
-	return "dummy_document_getElementById";
-    },
-};
-
-Element = {
-};
-
-HTMLElement = {
-    prototype: {},
-};
-
-
-Console = {}
+var Console = {};
 Console.puts = function(str, no_newline) {
-    print(str);
-    if (!no_newline) 
-	print("\n");
-}
+  print(str);
+  if (!no_newline) {
+    print("\n");
+  }
+};
 
 Console.p = function() {
-    print.apply(this, arguments)
-}
+  print.apply(this, arguments);
+};
 
 if(typeof(ev) != 'function')
     eval("function ev(str){ puts(str); return (new BiwaScheme.Interpreter()).evaluate(str); }");
--- src/server/node_preamble.js
+++ src/server/node_preamble.js
@@ -1,43 +1,6 @@
 var underscore = require('underscore');
 underscore.mixin(require('underscore.string'));
 
-var window = {
-};
-
-var navigator = {
-  userAgent: "console-nodejs"
-};
-
-var document = {
-  createElement: function(a) {
-    return {
-      appendChild: function() {
-        return "dummy_document_createElement_appendChild";
-      },
-    } ;
-  },
-  createTextNode: function(a) {
-    return "dummy_document_createTextNode";
-  },
-  createEvent: function(a) {
-    return "dummy_document_createEvent";
-  },
-  write: function(a) {
-    return "dummy_document_wirte";
-  },
-  getElementById: function(a) {
-    return "dummy_document_getElementById";
-  },
-};
-
-var Element = {
-};
-
-var HTMLElement = {
-  prototype: {}
-};
-
-
 var Console = {};
 Console.puts = function(str, no_newline) {
   require('sys').print(str);
