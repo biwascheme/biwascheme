c0543443baebb9bc08fc8b70ae6492348e6a7880
Ken Pratt <ken@kenpratt.net>
2011-07-27 12:29:37 -0700

    Cleaned up development_loader.

--- src/development_loader.js
+++ src/development_loader.js
@@ -1,48 +1,6 @@
 var BiwaScheme = BiwaScheme || {}; 
 
 (function(){ //local namespace
-
-  // readAttribute (taken from prototype.js 1.6.0)
-  var readAttribute = function(element, name) {
-    if (/*Prototype.Browser.IE*/ !!(window.attachEvent && !window.opera)){
-      var t = {
-        names: {
-          'class': 'className',
-          'for':   'htmlFor'
-        },
-        values: {
-          _getAttr: function(element, attribute) {
-            return element.getAttribute(attribute, 2);
-          },
-          _getAttrNode: function(element, attribute) {
-            var node = element.getAttributeNode(attribute);
-            return node ? node.value : "";
-          },
-          _getEv: function(element, attribute) {
-            var attribute = element.getAttribute(attribute);
-            return attribute ? attribute.toString().slice(23, -2) : null;
-          },
-          _flag: function(element, attribute) {
-            return $(element).hasAttribute(attribute) ? attribute : null;
-          },
-          style: function(element) {
-            return element.style.cssText.toLowerCase();
-          },
-          title: function(element) {
-            return element.title;
-          }
-        }
-      };
-      if (t.values[name]) return t.values[name](element, name);
-      if (t.names[name]) name = t.names[name];
-      if (name.indexOf(':') > -1){
-        return (!element.attributes || !element.attributes[name]) ? null :
-         element.attributes[name].value;
-      }
-    }
-    return element.getAttribute(name);
-  }
-
   // Find the script tag
   var find_script_tag = function(e){
     if(e.nodeName.toLowerCase() == 'script'){
@@ -52,22 +10,23 @@ var BiwaScheme = BiwaScheme || {};
       if(e.previousSibling.nodeName.toLowerCase() == 'script')
         return e.previousSibling;
       else
-        console.error("BiwaScheme could not find the script tag... please use firebug 1.5.0")
+        console.error("BiwaScheme could not find the script tag... please use firebug 1.5.0");
     }
     else{
       return find_script_tag(e.lastChild);
     }
   };
+
   var script = find_script_tag(document);
 
-  var src = readAttribute(script, 'src');
+  var src = script.getAttribute("src");
   var dir = src.match(/(.*)src\/development_loader.js/)[1];
 
   var script_tag = function(path){
     return '<script type="text/javascript" src="' +
              path +
            '"><\/script>';
-  }
+  };
   document.write(script_tag(dir+"src/version.js"));
   document.write(script_tag(dir+"src/jquery.js"));
   document.write(script_tag(dir+"src/underscore.js"));
