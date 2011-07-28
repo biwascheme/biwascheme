17d7d578965dbc7506ae251ba17713373866220b
Ken Pratt <ken@kenpratt.net>
2011-07-28 12:19:42 -0700

    Checked in current version of generated files.

--- src/version.js
+++ src/version.js
@@ -1,4 +1,4 @@
 var BiwaScheme = BiwaScheme || {};
 
 BiwaScheme.Version = "0.5.6";
-BiwaScheme.GitCommit = "dd2c006101fa62f686609045a96ec6a3f55a6b20";
+BiwaScheme.GitCommit = "bcc5682526fa97488b521fa2673a10fd654b82c1";
