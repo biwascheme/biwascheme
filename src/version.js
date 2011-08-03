11cefd4ee2f1ec43d2f2a010781386c3e8dd8f58
Ken Pratt <ken@kenpratt.net>
2011-08-02 18:25:08 -0700

    Checked in current version of generated files, updated node module in Socket.IO chat demo.

--- src/version.js
+++ src/version.js
@@ -1,4 +1,4 @@
 var BiwaScheme = BiwaScheme || {};
 
 BiwaScheme.Version = "0.5.7";
-BiwaScheme.GitCommit = "08149a03abf6ee077350e22f88a3548e17da34df";
+BiwaScheme.GitCommit = "11267b2503303b8a4e9731eb24b046d67ad175ef";
