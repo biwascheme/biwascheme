ceeab65e9e73815b508012e4c9dc124abf7bdc05
Yutaka HARA <yutaka.hara.gmail.com>
2011-08-06 18:09:20 +0900

    Regenerated files

--- src/version.js
+++ src/version.js
@@ -1,8 +1,4 @@
 var BiwaScheme = BiwaScheme || {};
 
 BiwaScheme.Version = "0.5.7";
-<<<<<<< HEAD
-BiwaScheme.GitCommit = "0e4cbc80ed4abcc327a71d8f98300f5b60d2d304";
-=======
-BiwaScheme.GitCommit = "e1d108943e475c8e9c1d6107c92f2379ba5e6393";
->>>>>>> 9ebe216613c4049578b7b0ae0a92382453c715e6
+BiwaScheme.GitCommit = "e145688f81e0844e1ff3543c69bf1612a516bf03";
