9473dc77372d9fbc376f431a4cec95cbae31ef7d
Yutaka HARA <yutaka.hara.gmail.com>
2011-12-02 02:26:31 +0900

    new: BiwaScheme.run_file (on Node)

--- src/platforms/node/module_postamble.js
+++ src/platforms/node/module_postamble.js
@@ -11,6 +11,13 @@ BiwaScheme.run = function(code) {
   });
   return intp.evaluate(code);
 };
+
+BiwaScheme.run_file = function(filename, encoding/*optional*/) {
+  var enc = encoding || 'utf8';
+  var src = require('fs').readFileSync(filename, enc);
+  return BiwaScheme.run(src);
+};
+
 BiwaScheme.define_libfunc("load", 1, 1, function(ar) {
   var relpath = ar[0];
   assert_string(relpath);
