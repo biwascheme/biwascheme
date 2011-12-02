fed02a49167130c4768faaacf7e758def83af4f0
Yutaka HARA <yutaka.hara.gmail.com>
2011-12-02 12:55:22 +0900

    refactor: use object syntax in src/platforms/node/platform.js

--- src/platforms/node/platform.js
+++ src/platforms/node/platform.js
@@ -1,47 +1,57 @@
 BiwaScheme.on_node = true;
 
-BiwaScheme.NodeJS.fs = require('fs');
-BiwaScheme.NodeJS.path = require('path');
-BiwaScheme.NodeJS.process = process;
-
 //
 // Node.js interface
 // see also: src/system/_nodejs.js
 //
-// Chapter 9 File system
+_.extend(BiwaScheme.NodeJS, {
+  fs: require('fs'),
+  path: require('path'),
+  process: process,
 
-// Check the file exists.
-//
-// filename - string
-// Returns true or false.
-BiwaScheme.NodeJS.file_exists = function(filename){
-  return this.path.existsSync(filename);
-};
+  //
+  // R6RS Stdlib 9 File system
+  // 
 
-// Delete a file.
-//
-// filename - string
-// Returns undefined.
-BiwaScheme.NodeJS.delete_file = function(filename){
-  this.fs.unlinkSync(filename);
-};
+  // Check the file exists.
+  //
+  // filename - string
+  // Returns true or false.
+  file_exists: function(filename){
+    return this.path.existsSync(filename);
+  };
 
-// Chapter 10 Command-line access and exit values
+  // Delete a file.
+  //
+  // filename - string
+  // Returns undefined.
+  delete_file: function(filename){
+    this.fs.unlinkSync(filename);
+  };
 
-// Returns array of strings.
-// First element is the process name, rest are command-line arguments.
-BiwaScheme.NodeJS.command_line = function(){
-  return this.process.argv;
-};
+  //
+  // R6RS Stdlib 10 Command-line access and exit values
+  //
 
-// Exits the running program.
-// obj - Exit code (default: 0)
-//       When false(#f) is given, the exit is assumed to be abnormal.
-BiwaScheme.NodeJS.exit = function(obj){
-  var code = _.isUndefined(obj) ? 0 :
-             (obj === false)    ? 1 :
-             Number(obj);
+  // Returns array of strings.
+  // First element is the process name, rest are command-line arguments.
+  command_line: function(){
+    return this.process.argv;
+  };
 
-  this.process.exit(code);
+  // Exits the running program.
+  // obj - Exit code (default: 0)
+  //       When false(#f) is given, the exit is assumed to be abnormal.
+  exit: function(obj){
+    var code = _.isUndefined(obj) ? 0 :
+               (obj === false)    ? 1 :
+               Number(obj);
+
+    this.process.exit(code);
+  }
+
+  //
+  // SRFI 98 - get-environment-variable(s)
+  //
+  //BiwaScheme.NodeJS.
 }
-
