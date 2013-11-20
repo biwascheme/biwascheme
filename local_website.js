4e8f06932822d81f95c64a0ee032c4a24181f4e4
Yutaka HARA <yutaka.hara+github@gmail.com>
2013-11-20 18:33:14 +0900

    Add local_website.js

--- local_website.js
+++ local_website.js
@@ -1,10 +1,15 @@
 #!/usr/bin/env node
+//
+// local_website.js : Start http server on localhost:7001
+// (You can preview www.biwascheme.org with this)
+//
+
 var express = require('express'),
     app = require('express').createServer(),
     fs = require('fs');
 
 app.configure(function(){
-  app.use(express.static(__dirname + '/web'));
+  app.use(express.static(__dirname));
   //app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
