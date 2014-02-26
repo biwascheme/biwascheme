3bc9738a42a83f191207e341cadcca6a2c765a71
en <dasistdashexeneinmaleins@gmail.com>
2014-02-26 18:58:37 -0500

    expansion on the release_initializer.js shim for multiple scheme snippets
    details: status quo ante, only the body of the first <script> tag for biwascheme{,-min}.js would be executed.
    
    changes: now execute a) the body of *every* <script> tag to a file with a name containing "biwascheme" (this may need rethinking), as well as b) the src (if any) of every <script> tag with the class "biwascheme", or the body otherwise.

--- src/platforms/browser/release_initializer.js
+++ src/platforms/browser/release_initializer.js
@@ -1,37 +1,58 @@
-//
 // release_initializer.js - read user's program and eval it (if it exists)
-//
 // This file is put on the end the lib/biwascheme.js.
-//
-(function(){ //local namespace
-  var dumper = null;
-  if ($("#biwascheme-debugger")[0]) {
-    dumper = new BiwaScheme.Dumper($("#biwascheme-debugger")[0]);
-  }
+$(document).ready(function () {
+  "use strict";
+
+  var dumper = (function () {
+    if ($("#biwascheme-debugger")[0]) {
+      return new BiwaScheme.Dumper($("#biwascheme-debugger")[0]);
+    }
+    else {
+      return null;
+    }
+  })();
 
   // Error handler (show message to console div)
-  var onError = function(e, state){
+  var onError = function (e, state) {
     puts(e.message);
+
     if (dumper) {
       dumper.dump(state);
       dumper.dump_move(1);
-    } else if (typeof(console) !== "undefined" && console.error) {
+    }
+    else if (typeof console !== "undefined" && console.error) {
       console.error(e.message);
-    } else {
-      throw(e);
+    }
+    else {
+      throw e;
+    }
+  };
+
+  var interpret = function (script) {
+    var interp = new BiwaScheme.Interpreter(onError);
+
+    try {
+      interp.evaluate(script, function () {});
+    }
+    catch (e) {
+      onError(e);
     }
   };
 
   // Start user's program
-  var script = $("script[src$='biwascheme.js']").html() ||
-               $("script[src$='biwascheme-min.js']").html();
-  if (script) {
-    var intp = new BiwaScheme.Interpreter(onError);
-    try{
-      intp.evaluate(script, function(){});
+  $("script[src*='biwascheme']").each(function (ix) {
+    interpret($(this).html());
+  });
+
+  $("script.biwascheme").each(function (ix) {
+    var elem = $(this);
+    var source = elem.attr("src");
+
+    if (source) {
+      $.get(source, interpret, "text");
     }
-    catch(e){
-      onError(e);
+    else {
+      interpret(elem.html());
     }
-  }
-})();
+  });
+});
