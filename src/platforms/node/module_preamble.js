111bf03b893af58d2ba529fa5d013900c1855702
Yutaka HARA <yutaka.hara+github@gmail.com>
2016-05-30 20:51:40 +0900

    fix: remove unused function

--- src/platforms/node/module_preamble.js
+++ src/platforms/node/module_preamble.js
@@ -13,10 +13,6 @@ Console.p = function() {
   require('util').print.apply(this, arguments);
 };
 
-if(typeof(ev) != 'function') {
-  eval("function ev(str){ puts(str); return (new BiwaScheme.Interpreter()).evaluate(str); }");
-}
-
 if(typeof(BiwaScheme) == "undefined") BiwaScheme = {};
 BiwaScheme.on_node = true;
 BiwaScheme._ = _;
