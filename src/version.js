142eb60a007caa75acfe454988eeb3c49f3ee950
Yutaka HARA <yutaka.hara+github@gmail.com>
2019-12-18 23:49:40 +0900

    fix: Add src/version.js
    refs #150

---
+++ src/version.js
@@ -0,0 +1,12 @@
+/*
+ * BiwaScheme 0.6.9 - R6RS Scheme in JavaScript
+ *
+ * Copyright (c) 2007-2014 Yutaka HARA (http://www.biwascheme.org/)
+ * @license Licensed under the MIT license.
+ */
+
+var BiwaScheme = BiwaScheme || {};
+
+BiwaScheme.Version = "0.6.9";  // TODO: deprecate this
+BiwaScheme.VERSION = "0.6.9";
+BiwaScheme.GitCommit = "d7985e08b9e341a2a521d6ae84accb128d1438d6";
