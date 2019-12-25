53c1a0906f06af0af18d1d79407b17e7f4ce63cc
Yutaka HARA <yutaka.hara+github@gmail.com>
2019-12-25 23:08:29 +0900

    fix: Fix define-macro

--- src/library/extra_lib.js
+++ src/library/extra_lib.js
@@ -238,11 +238,12 @@ if( typeof(BiwaScheme)!='object' ) BiwaScheme={}; with(BiwaScheme) {
       expected_args = lambda.cdr.car;
     }
 
-    //[close, n_frees, do_body, next]
+    //["close", <args>, <n>, <body>, <opecodes_next>, <dotpos>]
     var opc = Compiler.compile(lambda);
-    if(opc[1] != 0)
+    if(opc[2] != 0)
       throw new Bug("you cannot use free variables in macro expander (or define-macro must be on toplevel)")
-    var cls = [opc[2]];
+    var cls = [opc[3]];
+    cls.closure_p = true;
 
     TopEnv[name.name] = new Syntax(name.name, function(sexp){
       var given_args = sexp.to_array();
