245095037328c8c7ccc1572ebe0153f0e19dd751
Yutaka HARA <yutaka.hara+github@gmail.com>
2020-06-23 23:46:44 +0900

    refactor: Remove platform.js
    Now we don't need it because we've split main-*.js

--- src/platforms/platform.js
+++
@@ -1,8 +0,0 @@
-const Platform = {
-  // Return true if running on Node.js
-  isNode: () => typeof global !== 'undefined',
-  // Return true if running on a browser
-  isBrowser: () => typeof window !== 'undefined',
-}
-
-export default Platform;
