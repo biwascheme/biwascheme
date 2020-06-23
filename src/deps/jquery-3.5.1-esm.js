bf6ac01142e2fa67f23522d91b82aa19dabf5038
Yutaka HARA <yutaka.hara+github@gmail.com>
2020-06-23 23:05:08 +0900

    fix: Avoid warning of rollup -c
    > (!) `this` has been rewritten to `undefined`

--- src/deps/jquery-3.5.1-esm.js
+++ src/deps/jquery-3.5.1-esm.js
@@ -38,7 +38,8 @@ export default ( function( global, factory ) {
 //	}
 
 // Pass this if window is not defined yet
-} )( typeof window !== "undefined" ? window : this, function( window, noGlobal ) {
+//} )( typeof window !== "undefined" ? window : this, function( window, noGlobal ) {
+  } )( typeof window !== "undefined" ? window : undefined, function( window, noGlobal ) {
 
 // Edge <= 12 - 13+, Firefox <=18 - 45+, IE 10 - 11, Safari 5.1 - 9+, iOS 6 - 9.1
 // throw exceptions when non-strict code (e.g., ASP.NET 4.5) accesses strict mode
