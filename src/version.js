4d326702b7c25a6a2de16df7bbce095940f4fa50
Yutaka HARA <yutaka.hara+github@gmail.com>
2020-06-26 00:15:23 +0900

    fix: Set BiwaScheme.VERSION

--- src/version.js
+++ src/version.js
@@ -1,12 +1,2 @@
-/*
- * BiwaScheme 0.7.0 - R6RS Scheme in JavaScript
- *
- * Copyright (c) 2007-2014 Yutaka HARA (http://www.biwascheme.org/)
- * @license Licensed under the MIT license.
- */
-
-var BiwaScheme = BiwaScheme || {};
-
-BiwaScheme.Version = "0.7.0";  // TODO: deprecate this
-BiwaScheme.VERSION = "0.7.0";
-BiwaScheme.GitCommit = "7e2de46a86d131158b8716c915c1ddbc66edd5da";
+export const VERSION = "0.7.0";
+export const GitCommit = "3c117d2ade60b5ef77f0170a3b3106d5eae8915a";
