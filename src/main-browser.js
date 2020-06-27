5fd8104f5fa3b574d7f5c89c0adefa8c30a76021
Yutaka HARA <yutaka.hara+github@gmail.com>
2020-06-27 23:48:54 +0900

    fix: Put .on_node
    Biwa seems not using this, but for backward compatibility

--- src/main-browser.js
+++ src/main-browser.js
@@ -4,3 +4,4 @@ import { jsonp_receiver } from "./library/webscheme_lib.js"
 window.BiwaScheme = window.BiwaScheme || {};
 Object.assign(window.BiwaScheme, BiwaScheme);
 window.BiwaScheme.jsonp_receiver = jsonp_receiver;
+window.BiwaScheme.on_node = false;
--- src/main-node.js
+++ src/main-node.js
@@ -1,6 +1,7 @@
 import BiwaScheme from "./main.js";
 import { run, run_file, node_setup } from "./platforms/node/module_postamble.js"
 
+BiwaScheme.on_node = true;
 BiwaScheme.run = run;
 BiwaScheme.run_file = run_file;
 node_setup(BiwaScheme);
