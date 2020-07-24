ea72749207cc2a049f7c826fd5721299698cac6b
Yutaka HARA <yutaka.hara+github@gmail.com>
2020-07-25 08:14:14 +0900

    style: Add comment

--- src/main-browser.js
+++ src/main-browser.js
@@ -1,9 +1,6 @@
 import BiwaScheme from "./main.js";
 BiwaScheme.on_node = false;
 
-import { jsonp_receiver } from "./library/webscheme_lib.js"
-BiwaScheme.jsonp_receiver = jsonp_receiver;
-
 import Console from "./platforms/node/console.js"
 BiwaScheme.Console = Console;
 
@@ -11,6 +8,13 @@ import { current_input, current_output, current_error } from "./platforms/browse
 BiwaScheme.Port.current_input = current_input;
 BiwaScheme.Port.current_output = current_output;
 BiwaScheme.Port.current_error = current_error;
+
+//
+// browser-specific code
+//
+
+import { jsonp_receiver } from "./library/webscheme_lib.js"
+BiwaScheme.jsonp_receiver = jsonp_receiver;
 
 // TODO: ideally this should just be `window.BiwaScheme = BiwaScheme` but it will break test/spec.html (grep with `register_tests`)
 window.BiwaScheme = window.BiwaScheme || {};
--- src/main-node.js
+++ src/main-node.js
@@ -1,12 +1,6 @@
 import BiwaScheme from "./main.js";
 BiwaScheme.on_node = true;
 
-import "./platforms/node/node_functions.js"
-
-import { run, run_file } from "./platforms/node/run.js"
-BiwaScheme.run = run;
-BiwaScheme.run_file = run_file;
-
 import Console from "./platforms/node/console.js"
 BiwaScheme.Console = Console;
 
@@ -14,5 +8,15 @@ import { current_input, current_output, current_error } from "./platforms/node/d
 BiwaScheme.Port.current_input = current_input;
 BiwaScheme.Port.current_output = current_output;
 BiwaScheme.Port.current_error = current_error;
+
+//
+// node-specific code
+//
+
+import "./platforms/node/node_functions.js"
+
+import { run, run_file } from "./platforms/node/run.js"
+BiwaScheme.run = run;
+BiwaScheme.run_file = run_file;
 
 module.exports = BiwaScheme;
