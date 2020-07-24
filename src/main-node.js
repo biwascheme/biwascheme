220157bc9c9ed23e1790d9361b6578d6bc8f8065
Yutaka HARA <yutaka.hara+github@gmail.com>
2020-07-24 19:10:18 +0900

    feat(node): Load node_functions

--- src/main-node.js
+++ src/main-node.js
@@ -1,6 +1,8 @@
 import BiwaScheme from "./main.js";
 BiwaScheme.on_node = true;
 
+import "./platforms/node/node_functions.js"
+
 import { run, run_file, node_setup } from "./platforms/node/module_postamble.js"
 BiwaScheme.run = run;
 BiwaScheme.run_file = run_file;
