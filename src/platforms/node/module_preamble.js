2eea95fdfd5e89d27ec39a7cdf896d47b6ae9327
Yutaka HARA <yutaka.hara+github@gmail.com>
2020-07-24 18:09:27 +0900

    cleanup: Remove node/module_preamble.js

--- src/platforms/node/module_preamble.js
+++
@@ -1,9 +0,0 @@
-var _ = require('underscore');
-var _$1 = require('underscore');
-
-var Console = {};
-
-//if(typeof(BiwaScheme) == "undefined") BiwaScheme = {};
-//BiwaScheme.on_node = true;
-//BiwaScheme._ = _;
-//BiwaScheme.Console = Console;
