15644911d3462088357055041c7769a88e6c5391
Yutaka HARA <yutaka.hara+github@gmail.com>
2020-08-14 00:06:24 +0900

    feat: Add optional form to BiwaError

--- src/system/error.js
+++ src/system/error.js
@@ -1,13 +1,16 @@
-import Class from "./class.js"
 import * as _ from "../deps/underscore-1.10.2-esm.js"
+import { to_write } from "./_writer.js"
+import Class from "./class.js"
 
 //
 // Errors
 //
 
 const BiwaError = Class.create({
-  initialize: function(msg){
-    this.message = "Error: "+msg;
+  initialize: function(msg, form=null){
+    const info = (form === null ? "" : `: ${to_write(form)}`);
+    this.message = `Error: ${msg}${info}`;
+    this.form = form;
   },
   toString: function(){
     return this.message;
