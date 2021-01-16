b25e360cf24af3fe61b0ee20ab567e0defa1d7d6
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2021-01-16 13:12:03 -0800

    Merge rollup config into one file and add build command

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,46 @@
-import { terser } from "rollup-plugin-terser"
+import prettier from "rollup-plugin-prettier";
+import { terser } from "rollup-plugin-terser";
 import { VERSION } from "./src/version.js";
 
 const banner = `/*
  * BiwaScheme ${VERSION} - R6RS/R7RS Scheme in JavaScript
  *
- * Copyright (c) 2007-${(new Date).getFullYear()} Yutaka HARA (http://www.biwascheme.org/)
+ * Copyright (c) 2007-${new Date().getFullYear()} Yutaka HARA (http://www.biwascheme.org/)
  * Licensed under the MIT license.
- */`
+ */`;
 
-export default {
-  input: "src/main-browser.js",
-  output: [
-    {
-      file: "release/biwascheme.js",
-      format: "iife",
-      name: "BiwaScheme",
-      strict: false,
-      banner: banner,
-    },
-    {
-      file: "release/biwascheme-min.js",
-      format: "iife",
-      name: "BiwaScheme",
-      strict: false,
-      banner: banner,
-      plugins: [terser({output: {comments: /Copyright/}})],
-    },
-  ]
-}
+export default [
+  {
+    plugins: [prettier({})],
+    input: "src/main-node.js",
+    output: [
+      {
+        file: "release/node_biwascheme.js",
+        format: "cjs",
+        name: "BiwaScheme",
+        strict: false,
+        plugins: [prettier()],
+      },
+    ],
+  },
+  {
+    input: "src/main-browser.js",
+    output: [
+      {
+        file: "release/biwascheme.js",
+        format: "iife",
+        name: "BiwaScheme",
+        strict: false,
+        banner: banner,
+      },
+      {
+        file: "release/biwascheme-min.js",
+        format: "iife",
+        name: "BiwaScheme",
+        strict: false,
+        banner: banner,
+        plugins: [terser({ output: { comments: /Copyright/ } })],
+      },
+    ],
+  },
+];
