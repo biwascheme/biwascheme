7cb105dcbfc384fcf9ffbd4d081b6c4a8091c26e
Yutaka HARA <yutaka.hara+github@gmail.com>
2021-08-09 13:09:01 +0900

    chores: Reduce circular dependency
    refs #211

--- src/system/_writer.js
+++ src/system/_writer.js
@@ -2,7 +2,6 @@ import * as _ from "../deps/underscore-esm.js"
 import { isVector } from "./_types.js"
 import { nil, undef } from "../header.js"
 import { BiwaSymbol } from "./symbol.js"
-import { isPair } from "./pair.js"
 
 //
 // _writer.js: Functions to convert objects to strings
@@ -155,7 +154,7 @@ function _gather_information(obj, state) {
   }
   // Found a new object
   state.objs.add(obj);
-  if (isPair(obj)) {
+  if (BiwaScheme.isPair(obj)) { // Call via `BiwaScheme` to avoid circular dependency warning (#211)
     state.parents.add(obj);
     _gather_information(obj.car, state);
     _gather_information(obj.cdr, state);
