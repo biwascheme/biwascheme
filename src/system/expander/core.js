3fd512e689a58041a9c6a912233d0e2c533dcfab
Yutaka HARA <yutaka.hara+github@gmail.com>
2022-05-24 11:06:42 +0900

    refactor: Sort by alphabetic order

--- src/system/expander/core.js
+++ src/system/expander/core.js
@@ -5,54 +5,13 @@ import { List, Cons, isPair, isList } from "../pair.js"
 import { Sym } from "../symbol.js"
 import { isIdentifier, unwrapSyntax } from "./syntactic_closure.js"
 
-const expandLambda = async ([form, xp]) => {
-  const err = new BiwaError("malformed lambda", form);
-  const l = form.to_array();
-  if (l.length < 3) throw err;
-  const formals = form.cadr(err);
-  if (!isPair(formals)) throw err;
-  const body = form.cddr(err);
-
-  const newEnv = env.extend(formal-list);
-
-  // TODO
-
-  return Cons(Sym("lambda"),
-           Cons(newFormals, newBody));
+// `begin`
+const expandBegin = async ([form, xp]) => {
+  const body = await form.cdr.mapAsync(item => xp.expand(item));
+  return Cons(Sym("begin"), body);
 };
 
-const expandIf = async ([form, xp]) => {
-  const l = form.to_array();
-  switch (l.length) {
-    case 3:
-      return List(Sym("if"), await xp.expand(l[1]), await xp.expand(l[2]));
-    case 4:
-      return List(Sym("if"), await xp.expand(l[1]), await xp.expand(l[2]), await xp.expand(l[3]));
-    default:
-      throw new BiwaError("malformed if", form);
-  }
-};
-
-const expandQuote = async ([form, xp]) => {
-  const l = form.to_array();
-  switch (l.length) {
-    case 2:
-      return List(Sym("quote"), unwrapSyntax(l[1]));
-    default:
-      throw new BiwaError("malformed quote", form);
-  }
-};
-
-const expandSet = async ([form, xp]) => {
-  const l = form.to_array();
-  switch (l.length) {
-    case 3:
-      return List(Sym("set!"), await xp.expand(l[1]), await xp.expand(l[2]));
-    default:
-      throw new BiwaError("malformed set!", form);
-  }
-};
-
+// `call/cc`
 const expandCallCc = async ([form, xp]) => {
   const l = form.to_array();
   switch (l.length) {
@@ -63,11 +22,7 @@ const expandCallCc = async ([form, xp]) => {
   }
 };
 
-const expandBegin = async ([form, xp]) => {
-  const body = await form.cdr.mapAsync(item => xp.expand(item));
-  return Cons(Sym("begin"), body);
-};
-
+// `define`
 // Destructively modifies `env`
 const expandDefine = async ([form, xp, env]) => {
   const l = form.to_array();
@@ -87,6 +42,58 @@ const expandDefine = async ([form, xp, env]) => {
   }
 };
 
+// `if`
+const expandIf = async ([form, xp]) => {
+  const l = form.to_array();
+  switch (l.length) {
+    case 3:
+      return List(Sym("if"), await xp.expand(l[1]), await xp.expand(l[2]));
+    case 4:
+      return List(Sym("if"), await xp.expand(l[1]), await xp.expand(l[2]), await xp.expand(l[3]));
+    default:
+      throw new BiwaError("malformed if", form);
+  }
+};
+
+// `lambda`
+const expandLambda = async ([form, xp]) => {
+  const err = new BiwaError("malformed lambda", form);
+  const l = form.to_array();
+  if (l.length < 3) throw err;
+  const formals = form.cadr(err);
+  if (!isPair(formals)) throw err;
+  const body = form.cddr(err);
+
+  const newEnv = env.extend(formal-list);
+
+  // TODO
+
+  return Cons(Sym("lambda"),
+           Cons(newFormals, newBody));
+};
+
+// `quote`
+const expandQuote = async ([form, xp]) => {
+  const l = form.to_array();
+  switch (l.length) {
+    case 2:
+      return List(Sym("quote"), unwrapSyntax(l[1]));
+    default:
+      throw new BiwaError("malformed quote", form);
+  }
+};
+
+// `set!`
+const expandSet = async ([form, xp]) => {
+  const l = form.to_array();
+  switch (l.length) {
+    case 3:
+      return List(Sym("set!"), await xp.expand(l[1]), await xp.expand(l[2]));
+    default:
+      throw new BiwaError("malformed set!", form);
+  }
+};
+
 // TODO
 // const expandDefineRecordType
 // const expandParameterize
@@ -101,12 +108,12 @@ const expandDefine = async ([form, xp, env]) => {
 
 // Install core expanders into `lib`
 const installCore = (lib) => {
-  lib.exportMacro(Sym("lambda"), expandLambda);
+  lib.exportMacro(Sym("begin"), expandBegin);
+  lib.exportMacro(Sym("call/cc"), expandCallCc);
+  lib.exportMacro(Sym("define"), expandDefine);
   lib.exportMacro(Sym("if"), expandIf);
+  lib.exportMacro(Sym("lambda"), expandLambda);
   lib.exportMacro(Sym("quote"), expandQuote);
   lib.exportMacro(Sym("set!"), expandSet);
-  lib.exportMacro(Sym("call/cc"), expandCallCc);
-  lib.exportMacro(Sym("begin"), expandBegin);
-  lib.exportMacro(Sym("define"), expandDefine);
 };
 export { installCore };
