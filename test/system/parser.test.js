125b26dcdc132514c067583b41cd9cf8da863db0
Yutaka Hara <yutaka.hara+github@gmail.com>
2024-04-17 18:09:39 +0900

    Add _getToken()
    to handle number-like ident (`1+`, etc.) properly

--- test/system/parser.test.js
+++ test/system/parser.test.js
@@ -1,5 +1,7 @@
 import { nil } from "../../src/header.js"
 import { Pair } from "../../src/system/pair.js"
+import { Sym } from "../../src/system/symbol.js"
+import { Char } from "../../src/system/char.js"
 import Parser from "../../src/system/parser.js"
 
 function parse(txt) {
@@ -25,5 +27,75 @@ describe("Parser", () => {
 
   test("nil", () => {
     expect(parse("()")).toBe(nil);
+  })
+
+  describe("symbols", () => {
+    test("simple", () => {
+      expect(parse("foo")).toBe(Sym("foo"));
+    })
+
+    test("starts with number", () => {
+      expect(parse("1+")).toBe(Sym("1+"));
+    })
+
+    test("whitespace", () => {
+      expect(parse('| |')).toBe(Sym(' '));
+    })
+  })
+
+  describe("strings", () => {
+    test("simple", () => {
+      expect(parse('"hello"')).toBe("hello");
+    })
+
+    test("whitespace", () => {
+      expect(parse('"hello world"')).toBe("hello world");
+    })
+
+    test("tab", () => {
+      expect(parse('"hello\tworld"')).toBe("hello\tworld");
+    })
+
+    test("newline", () => {
+      expect(parse('"hello\nworld"')).toBe("hello\nworld");
+    })
+  })
+
+  describe("chars", () => {
+    test("simple", () => {
+      expect(parse("#\\a")).toBe(Char.get("a"));
+    })
+
+    test("number", () => {
+      expect(parse("#\\1")).toBe(Char.get("1"));
+    })
+
+    test("paren", () => {
+      expect(parse("#\\(")).toBe(Char.get("("));
+    })
+
+    test("backslash", () => {
+      expect(parse("#\\\\")).toBe(Char.get("\\"));
+    })
+
+    test("whitespace", () => {
+      expect(parse("#\\ ")).toBe(Char.get(" "));
+    })
+
+    test("tab", () => {
+      expect(parse("#\\\t")).toBe(Char.get("\t"));
+    })
+
+    test("newline", () => {
+      expect(parse("#\\\n")).toBe(Char.get("\n"));
+    })
+
+    test("named", () => {
+      expect(parse("#\\newline")).toBe(Char.get("\n"));
+    })
+
+    test("unknown name", () => {
+      expect(() => parse("#\\foo")).toThrow(Parser.Invalid)
+    })
   })
 })
